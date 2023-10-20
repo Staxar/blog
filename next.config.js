9c1968827ad8d9ba9558b1b5684c9bd44b8fc480
madomaga <marcin.domagala@capgemini.com>
2023-10-20 16:05:02 +0200

    mdx

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
+const withMDX = require("@next/mdx")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   env: {
     BASE_URL: process.env.BASE_URL,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
---
+++ src/app/mdx/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import Content from "../content/content.mdx";
+
+export default function ShouldWork() {
+  return <Content />;
+}
