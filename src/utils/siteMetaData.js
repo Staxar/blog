691b6ede55040f67acf87673c283ad73645f6f84
madomaga <marcin.domagala@capgemini.com>
2023-11-03 15:49:56 +0100

    seo

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,18 @@
+const siteMetadata = {
+  title: "Next.js Blog With Tailwind CSS and Contentlayer",
+  author: "Staxar",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind.css and contentlayer.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "marcin.kepno@gmail.com",
+  github: "https://github.com/Staxar",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/marcin-domaga%C5%82a-17a003251/",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
