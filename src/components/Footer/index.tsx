04e6571b6855e3e5a42e8452d0dd8c62f9848b22
Staxar <marcin.kepno@gmail.com>
2024-06-07 12:18:09 +0200

    styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import siteMetadata from "../../utils/siteMetaData";
 function Footer() {
   return (
-    <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light ">
-      <h3 className="mt-16 font-medium text-center capitalize text-4xl px-4">
+    <footer className="mt-16 rounded-2xl bg-dark m-4 flex flex-col items-center text-light ">
+      <h3 className="mt-16 font-medium text-center capitalize text-md md:text-4xl px-4">
         Interesting Stories | Updates | Guides
       </h3>
       <div className="flex items-center mt-8">
@@ -34,12 +34,17 @@ function Footer() {
           <GithubIcon className="hover:scale-125 transition-all ease duration-200 fill-light" />
         </a>
       </div>
-      <div className="w-full mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-row items-center justify-between">
-        <span>&copy; 2023. All right reserved</span>
-        <Link href={"/sitemap.xml"} className="text-center underline">
+      <div className="w-full mt-12 relative font-medium border-t border-solid border-light py-6 px-4 md:px-8 flex flex-row items-center justify-between">
+        <span className="text-xs md:text-md">
+          &copy; 2023. All right reserved
+        </span>
+        <Link
+          href={"/sitemap.xml"}
+          className="text-center underline text-xs md:text-md"
+        >
           sitemap.xml
         </Link>
-        <div className="text-center">Made by Staxar</div>
+        <div className="text-center text-xs md:text-md">Made by Staxar</div>
       </div>
     </footer>
   );
